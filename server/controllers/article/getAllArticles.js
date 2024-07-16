956a161d16db89216f86b2d8e3014f29645fb538
Anshuman Sinha <anshumansinha2001@gmail.com>
2024-07-16 12:05:06 +0530

    cheking

--- server/controllers/article/getAllArticles.js
+++ server/controllers/article/getAllArticles.js
@@ -2,18 +2,18 @@ const Article = require("../../models/article");
 
 const getAllArticles = async (req, res) => {
   try {
-    // Fetch all articles sorted by creation date
     const articles = await Article.find().sort({ createdAt: -1 });
 
-    // Adjust articles to include full image URLs
-    const formattedArticles = articles.map((article) => ({
-      ...article.toObject(),
-      img: article.img
-        ? `${req.protocol}://${req.headers.host}/${article.img}`
-        : null,
-    }));
+    let filteredArticles = articles;
+    if (req.query.search) {
+      filteredArticles = articles.filter(
+        (article) =>
+          article.content.includes(req.query.search) ||
+          article.category.includes(req.query.search)
+      );
+    }
 
-    res.status(200).json(formattedArticles);
+    res.status(200).json(filteredArticles);
   } catch (error) {
     res.status(500).json({ message: "Failed to retrieve articles" });
     console.error(error.message);
